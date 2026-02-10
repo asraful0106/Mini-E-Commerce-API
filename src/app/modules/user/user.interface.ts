a2f8c62c1c79df95a621fe859291e863abea3721
asraful0106 <asrafulapf@gmail.com>
2026-02-10 12:18:41 +0600

    user interface created

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,44 @@
+import type { Types } from "mongoose";
+
+export enum UserRole {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  CUSTOMER = "CUSTOMER",
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IAuthProvider {
+  provider: string;
+  providerId: string;
+}
+
+export interface IUserImage {
+  url: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export interface IUser {
+  _id: Types.ObjectId;
+  name: string;
+  phone?: string | null;
+  email: string;
+  password: string;
+  address?: string | null;
+  is_verified: boolean;
+  role: UserRole;
+  cancelled_orders_count: number;
+  isDeleted?: boolean;
+  isActive?: IsActive;
+  image: IUserImage;
+  auth: IAuthProvider[];
+  cart: Types.ObjectId[];
+  orders: Types.ObjectId[];
+  created_at: Date;
+  updated_at: Date;
+}
